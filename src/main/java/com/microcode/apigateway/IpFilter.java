e68f61477c1b069019d0a4a7808bd5dc9bca1325
JAVIER CAMILO GONZALEZ RODRIGUEZ <javgonzalez@grupoasa.local>
2025-04-03 18:27:20 -0500

    Actualización

--- src/main/java/com/microcode/apigateway/IpFilter.java
+++ src/main/java/com/microcode/apigateway/IpFilter.java
@@ -1,38 +1,38 @@
-package com.microcode.apigateway;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Objects;
-import java.util.Set;
-
-@Configuration
-public class IpFilter implements WebFilter {
-    private static final Set<String> ALLOWED_IPS = Set.of(resolveIp("portal.genialw.com"));
-
-    private static String resolveIp(String hostname) {
-        try {
-            return InetAddress.getByName(hostname).getHostAddress();
-        } catch (UnknownHostException e) {
-            throw new RuntimeException("No se pudo resolver el dominio", e);
-        }
-    }
-
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        String clientIp = Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress();
-
-        if (!ALLOWED_IPS.contains(clientIp)) {
-            exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
-            return exchange.getResponse().setComplete();
-        }
-
-        return chain.filter(exchange);
-    }
-}
+//package com.microcode.apigateway;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.server.ServerWebExchange;
+//import org.springframework.web.server.WebFilter;
+//import org.springframework.web.server.WebFilterChain;
+//import reactor.core.publisher.Mono;
+//
+//import java.net.InetAddress;
+//import java.net.UnknownHostException;
+//import java.util.Objects;
+//import java.util.Set;
+//
+//@Configuration
+//public class IpFilter implements WebFilter {
+//    private static final Set<String> ALLOWED_IPS = Set.of(resolveIp("portal.genialw.com"));
+//
+//    private static String resolveIp(String hostname) {
+//        try {
+//            return InetAddress.getByName(hostname).getHostAddress();
+//        } catch (UnknownHostException e) {
+//            throw new RuntimeException("No se pudo resolver el dominio", e);
+//        }
+//    }
+//
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+//        String clientIp = Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress();
+//
+//        if (!ALLOWED_IPS.contains(clientIp)) {
+//            exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+//            return exchange.getResponse().setComplete();
+//        }
+//
+//        return chain.filter(exchange);
+//    }
+//}
