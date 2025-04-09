92d096284cafbc867d8eb347f840fae6de88ffda
JAVIER CAMILO GONZALEZ RODRIGUEZ <javgonzalez@grupoasa.local>
2025-04-09 10:02:45 -0500

    Actualización

--- src/main/java/com/microcode/apigateway/CorsConfig.java
+++ src/main/java/com/microcode/apigateway/CorsConfig.java
@@ -23,7 +23,7 @@ public class CorsConfig {
 //        authCorsConfig.addAllowedOrigin("http://localhost:5174");
 //        authCorsConfig.addAllowedOrigin("http://localhost:5175");
         authCorsConfig.addAllowedOrigin("http://localhost:4200");
-//        authCorsConfig.addAllowedOrigin("http://localhost:8080");
+        authCorsConfig.addAllowedOrigin("http://localhost:8080");
         authCorsConfig.addAllowedMethod("*");
         authCorsConfig.addAllowedHeader("*");
         authCorsConfig.setMaxAge(3600L);
