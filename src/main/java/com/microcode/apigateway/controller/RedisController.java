120c30e698379222862c6174e57f32945e7acc07
JAVIER CAMILO GONZALEZ RODRIGUEZ <javgonzalez@grupoasa.local>
2025-09-06 09:07:32 -0500

    Actualización

--- src/main/java/com/microcode/apigateway/controller/RedisController.java
+++ src/main/java/com/microcode/apigateway/controller/RedisController.java
@@ -1,2 +1,33 @@
-package com.microcode.apigateway.controller;public class RedisController {
+package com.microcode.apigateway.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("/api/redis")
+@ResponseStatus(HttpStatus.OK)
+@AllArgsConstructor
+public class RedisController {
+
+    private final StringRedisTemplate redisTemplate;
+    private final ConcurrentHashMap<String, RateLimiter> rateLimiters = new ConcurrentHashMap<>();
+    private static final long BLOCK_DURATION_HOURS = 24;
+
+    @GetMapping("/is-block")
+    public boolean isBlocked(@RequestParam(defaultValue = "") String clientIp) {
+        String blockedKey = "blocked:" + clientIp;
+        return Boolean.TRUE.equals(redisTemplate.hasKey(blockedKey));
+    }
+
+    @PostMapping("/add-block")
+    public void addBlock(@RequestParam(defaultValue = "") String clientIp) {
+        String blockedKey = "blocked:" + clientIp;
+        redisTemplate.opsForValue().set(blockedKey, "true", BLOCK_DURATION_HOURS, TimeUnit.HOURS);
+    }
 }
